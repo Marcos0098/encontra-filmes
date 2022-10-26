41b376cb7435da1d9f01edabe8b8a6e449783a6f
Marcos0098 <115181147+Marcos0098@users.noreply.github.com>
2022-10-25 21:23:14 -0300

    adicionado title, post e rated

--- encontra-filmes/src/pages/Home.jsx
+++ encontra-filmes/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-
+import MovieCard from '../components/MovieCard';
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
@@ -8,8 +8,8 @@ const Home = () => {
   const [topMovies, setTopMovies] = useState([])
 
   const getTopRatedMovies = async (url) =>{
-    const res = await fetch(url)
-    const data = await res.json()
+    const res = await fetch(url);
+    const data = await res.json();
 
     setTopMovies(data.results);
   }
@@ -24,11 +24,11 @@ const Home = () => {
     <div className='container'>
       <h2 className='title'>Melhores Filmes:</h2>
       <div className="movies-container">
-        {topMovies === 0 && <p>Carregando...</p>}
-        {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
