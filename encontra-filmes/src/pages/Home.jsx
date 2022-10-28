0186940a8db9667acc185c7c423e44d374b07183
Marcos0098 <115181147+Marcos0098@users.noreply.github.com>
2022-10-27 21:50:06 -0300

    finalizado CSS da navbar/home

--- encontra-filmes/src/pages/Home.jsx
+++ encontra-filmes/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import MovieCard from '../components/MovieCard';
+
+import './MoviesGrid.css';
+
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
@@ -17,7 +20,7 @@ const Home = () => {
   useEffect(() => {
     const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
 
-    getTopRatedMovies(topRatedUrl);
+    getTopRatedMovies(topRatedUrl); 
   }, [])
 
   return (
