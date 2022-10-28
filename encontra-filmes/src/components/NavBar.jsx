78b29ab341ce3e93ad83c286714c0317c5f9fa2d
Marcos0098 <115181147+Marcos0098@users.noreply.github.com>
2022-10-28 17:57:30 -0300

    finalizado projeto encontra-filmes
    finalizado projeto para estudos de react usando Hooks de efeito e Rotas

--- encontra-filmes/src/components/NavBar.jsx
+++ encontra-filmes/src/components/NavBar.jsx
@@ -1,9 +1,21 @@
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2} from "react-icons/bi";
 
 import './NavBar.css';
 
 const NavBar = () => {
+  const [search, setSearch] = useState("")
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(!search) return
+
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  }
+
   return (
     <nav id="navbar">
         <h2>
@@ -11,8 +23,8 @@ const NavBar = () => {
             <BiCameraMovie />encontra-filmes
         </Link>
         </h2>
-        <form>
-            <input type="text" placeholder="Busque um Filme" />
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Busque um Filme" onChange={(e) => setSearch(e.target.value)} value={search} />
             <button type="submit">
                  <BiSearchAlt2 />    
             </button>
